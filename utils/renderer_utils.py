02a0a110cab43bf5b98fde667a260ca979d8476f
shengqi <shengqi-21@mails.tsinghua.edu.cn>
2025-04-21 13:11:46 +0000

    adjust render utils to remove custom raster

--- utils/renderer_utils.py
+++ utils/renderer_utils.py
@@ -3,16 +3,11 @@ from typing import Literal
 
 RendererNames = Literal[
     "diff_gaussian_rasterization",
-    "diff_gaussian_rasterization_custom",
     "forward_lite",
 ]
 
 __RENDERER_ATTRIBUTES = {
     "diff_gaussian_rasterization": {"forward_only": False, "support_vspace": False},
-    "diff_gaussian_rasterization_custom": {
-        "forward_only": False,
-        "support_vspace": True,
-    },
     "forward_lite": {"forward_only": True, "support_vspace": False},
 }
 
