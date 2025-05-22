8537bee375cb33304c74f7ef560b713083beed4a
Nielsencu <nielsencugito@gmail.com>
2025-05-22 12:26:26 +0000

    Finalize and tested segment dividing

--- scene/model/st_gaussian_model.py
+++ scene/model/st_gaussian_model.py
@@ -2,21 +2,8 @@ from scene.model.spacetime_gaussian_model import SpacetimeGaussianModel
 from utils.st_utils import SegmentTree, Interval
 from typing import NamedTuple
 import torch
-import time
 
-def get_gaussian_interval(opacity_activated : torch.Tensor, tcenter : torch.Tensor, tscale_activated : torch.Tensor, eps = 1e-5) -> Interval:
-    # Let the interval be [t_left, t_right].
-    # According to the Gaussian function, the opacity at time $t$ is
-    # $$O(t) = opcaity * \exp(-\frac{(t - tcenter)^2}{2 * tscale^2})$$
-    # In most cases, the Gaussian is considered to be invisible if
-    # $O(t) < 0.005$. Therefore, we can find the left and right bounds
-    # $$t_left = tcenter - \sqrt{-2 * tscale^2 * \log(0.005 / Opacity)}$$
-    # $$t_right = tcenter + \sqrt{-2 * tscale^2 * \log(0.005 / Opacity)}$$
-    # The interval is [t_left, t_right].
-    half = torch.where(opacity_activated > 0.005, tscale_activated * torch.sqrt(-2.0 * torch.log(0.005 / opacity_activated)), eps)
-    return torch.cat((tcenter - half, tcenter + half), dim=-1)
-
-
+# TODO: Instead of passing a gaussian model, pass just the attributes to segment_gaussian_to_st
 class GaussianAttrs(NamedTuple):
     xyz : torch.Tensor
     xyz_scales : torch.Tensor
@@ -62,7 +49,7 @@ class GaussianSegment:
 
 class STGWithSTModel(SpacetimeGaussianModel):
     def __init__(self, params: dict, context: dict):
-        self.max_level = 1
+        self.max_level = 3
         self.max_duration = 1.0
         self._st = SegmentTree(self.max_level, self.max_duration, GaussianSegment)
         super().__init__(params, context)
@@ -99,33 +86,7 @@ class STGWithSTModel(SpacetimeGaussianModel):
 
         print("Deallocated")
 
-        self._divide_to_segments()
-
-    def _divide_to_segments(self):
-        """
-        Move all gaussians to their respective segments, stored in self._st        
-        """
-        gaussian_segment_ids = self._get_segment_ids(self.opacity_activated, self.t, self.t_scale_active)
-        for i, segment in enumerate(self._st.get_all_segments_ref()):
-            segment_ids = (gaussian_segment_ids == i).nonzero().squeeze(-1)
-            segment.fill(*(getattr(self, attr)[segment_ids] for attr in self.attr_list))
-            print(f"Segment {i} size is {segment_ids.shape}")
-
-    def _get_segment_ids(self, opacity_activated : torch.Tensor, t_center : torch.Tensor, t_scale_activated : torch.Tensor):
-        gaussian_intervals = get_gaussian_interval(opacity_activated, t_center, t_scale_activated)
-        num_gaussians = opacity_activated.shape[0]
-        gaussian_segment_ids = torch.zeros(num_gaussians)
-        start_idx = 1
-        for level in range(1, self.max_level+1):
-            ranges = torch.linspace(0.0, self.max_duration, (2 ** level)+1)
-            bounds = torch.stack((ranges[:-1], ranges[1:]), dim=-1)
-            for i in range(len(bounds)):
-                bound = bounds[i]
-                is_in_bounds = (bound[0]  <= gaussian_intervals[:, 0]) & (bound[1] > gaussian_intervals[:, 1])
-                nonzero_indices = torch.nonzero(is_in_bounds)
-                gaussian_segment_ids[nonzero_indices] = start_idx + i
-            start_idx *= 2
-        return gaussian_segment_ids
+        segment_gaussians_to_st(self._st, self)
 
     def update(self, time : float):
         """
@@ -137,3 +98,42 @@ class STGWithSTModel(SpacetimeGaussianModel):
 
         self._set_gaussian_attributes_all({attr : gather_from_segments(attr).to(self.device) for attr in self.attr_list})
 
+def get_gaussian_interval(opacity_activated : torch.Tensor, tcenter : torch.Tensor, tscale_activated : torch.Tensor, eps = 1e-5) -> torch.Tensor:
+    # Let the interval be [t_left, t_right].
+    # According to the Gaussian function, the opacity at time $t$ is
+    # $$O(t) = opcaity * \exp(-\frac{(t - tcenter)^2}{2 * tscale^2})$$
+    # In most cases, the Gaussian is considered to be invisible if
+    # $O(t) < 0.005$. Therefore, we can find the left and right bounds
+    # $$t_left = tcenter - \sqrt{-2 * tscale^2 * \log(0.005 / Opacity)}$$
+    # $$t_right = tcenter + \sqrt{-2 * tscale^2 * \log(0.005 / Opacity)}$$
+    # The interval is [t_left, t_right].
+    half = torch.where(opacity_activated > 0.005, tscale_activated * torch.sqrt(-2.0 * torch.log(0.005 / opacity_activated)), eps)
+    return torch.cat((tcenter - half, tcenter + half), dim=-1)
+
+def segment_gaussians_to_st(segment_tree : SegmentTree, gaussian : STGWithSTModel) -> None:
+    """
+    Method returns None, fills in segment tree's segments
+    """
+
+    gaussian_intervals = get_gaussian_interval(gaussian.opacity_activated, gaussian.t, gaussian.t_scale_active)
+    num_gaussians = gaussian.opacity_activated.shape[0]
+    gaussian_segment_ids = torch.zeros(num_gaussians)
+    level_segment_start_id = 1
+    for level in range(1, segment_tree.max_level+1):
+        ranges = torch.linspace(0.0, segment_tree.duration, (2 ** level)+1)
+        bounds = torch.stack((ranges[:-1], ranges[1:]), dim=-1)
+        num_segment_bounds = len(bounds)
+        for segment_id in range(num_segment_bounds):
+            bound = bounds[segment_id]
+            is_in_bounds = (bound[0]  <= gaussian_intervals[:, 0]) & (bound[1] > gaussian_intervals[:, 1])
+            nonzero_indices = torch.nonzero(is_in_bounds)
+            segment_linear_id = level_segment_start_id + segment_id
+            gaussian_segment_ids[nonzero_indices] = segment_linear_id
+            
+        level_segment_start_id += num_segment_bounds
+        
+    print("Total number of gaussians ", gaussian.xyz.shape[0])
+    for i, segment in enumerate(segment_tree.get_all_segments_ref()):
+        segment_ids = (gaussian_segment_ids == i).nonzero().squeeze(-1)
+        segment.fill(*(getattr(gaussian, attr)[segment_ids] for attr in gaussian.attr_list))
+        print(f"Segment {i} size is {segment_ids.shape}")
